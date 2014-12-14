5fdb381906235cab997ebfa425577b9ef8beb880
falle38 <falle.malick@gmail.com>
2014-12-14 19:21:46 +0100

    implementation du controleur UserSpace

--- build/src/app/userspace/userspace.js
+++ build/src/app/userspace/userspace.js
@@ -1,7 +1,8 @@
 angular.module( 'ngBoilerplate.userspace', [
   'ui.router',
   'placeholders',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ngResource'
 ])
 
 .config(function config( $stateProvider ) {
@@ -17,7 +18,39 @@ angular.module( 'ngBoilerplate.userspace', [
   });
 })
 
-.controller( 'UseSpaceCtrl', function ProductsCtrl( $scope ) {
+.controller( 'UseSpaceCtrl', function UseSpaceCtrl( $scope, UserService ) {
+    
+    console.log("UseSpaceCtrl Instancié");
+        //$scope.UserService = UserService;
+        $scope.firstname = "Arthur";
+        $scope.lastname = "Clerc-Gherardi";
+        $scope.numphone = "06-99-05-00-10";
+        $scope.email = "acg@gmail.com";
+        $scope.adress="2 rue Catane 38000 GRENOBLE";
+        /*
+        var user = UserService.get();
+        $scope.firstname = user.fNameU;
+        $scope.lastname = user.lNameU;
+        $scope.numphone = user.phoneU;
+        $scope.email = user.mailU;
+        $scope.adress = user.adressU;
+        */
+                
+       
+    
 })
 
-;
+.factory('UserService', function ($http) {
+	var url = "users/sign";
+	return {
+		get: function(ur) {
+			return $http.get(url+"/find_user_by_email/"+ur);
+		},
+		getUserExist : function(ur){
+			return $http.get(url+"/find/"+ur);
+		},
+		post: function() {
+			return $http.post(url);
+		}
+		};
+	});
